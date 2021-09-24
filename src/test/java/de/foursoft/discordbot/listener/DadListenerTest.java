0062fdc8cc32dc14d74381e2382a99c555fa5e5e
DManstrator <dmanstrator@gmail.com>
2021-09-24 16:08:54 +0200

    Improved Mock Test and added dependency

--- src/test/java/de/foursoft/discordbot/listener/DadListenerTest.java
+++ src/test/java/de/foursoft/discordbot/listener/DadListenerTest.java
@@ -9,13 +9,15 @@ import org.mockito.Mock;
 
 import net.dv8tion.jda.api.entities.TextChannel;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.mockito.exceptions.base.MockitoException;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-//@ExtendWith(MockitoExtension.class)
+@ExtendWith(MockitoExtension.class)
 class DadListenerTest {
 
 	@Mock
@@ -26,11 +28,18 @@ class DadListenerTest {
 
 	@Test
 	void should_return_correct_message_in_channel() {
+		// ARRANGE
 		Message message = mock(Message.class);
 		when(guildMessageReceivedEvent.getMessage()).thenReturn(message);
 		when(message.getContentRaw()).thenReturn("ich bin dein Vater");
 		when(guildMessageReceivedEvent.getChannel()).thenReturn(channel);
 
-		verify(channel.sendMessage("Hi dein Vater, I'm dad"));
+		final DadListener dadListener = new DadListener();
+
+		// ACT
+		dadListener.accept(guildMessageReceivedEvent);
+
+		// ASSERT
+		verify(channel).sendMessage("Hi dein Vater, I'm dad");
 	}
 }
